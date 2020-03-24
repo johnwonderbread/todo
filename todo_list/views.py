c0ec1611539d4b835db09182edd185501fbd564b
johnwonderbread <tyler.johnathan@gmail.com>
2020-03-24 00:36:35 +0000

    70% of tutorial

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from .models import List
+from .forms import ListForm
 
 def home(request): 
-    return render(request, 'home.html', {})
+	if request.method == 'POST': 
+		form = ListForm(request.POST or None)
+		if form.is_valid():
+			form.save()
+			all_items = List.objects.all
+			return render(request, 'home.html', {'all_items': all_items})
+	else:
+		all_items = List.objects.all 
+		return render(request, 'home.html', {'all_items': all_items})
 
 def about(request):
 	context = {'first_name': 'John', 'last_name': 'Tyler'}

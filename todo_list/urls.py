2e2eec21f34f7dbe315c7b13c78ad41899a3aa91
johnwonderbread <tyler.johnathan@gmail.com>
2020-03-24 21:20:15 +0000

    End of course 1

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
 	path('', views.home, name='home'),
 	path('about/', views.about, name='about'),
+	path('delete/<list_id>', views.delete, name='delete'),
+	path('cross_off/<list_id>', views.cross_off, name='cross_off'),
+	path('uncross_off/<list_id>', views.uncross_off, name='uncross_off'),
+	path('edit/<list_id>', views.edit, name='edit'),
 ]
  
